63cb9b3b4b6dd3b07d9258e34673dde2b480fa8c
Victor Alexe <victor.alexe@stud.acs.upb.ro>
2024-10-15 20:04:46 +0300

    src: Solve m2::Lab3()
    Signed-off-by: Victor Alexe <victor.alexe@stud.acs.upb.ro>

--- src/main.cpp
+++ src/main.cpp
@@ -44,19 +44,7 @@ int main(int argc, char **argv)
     (void)Engine::Init(wp);
 
     // Create a new 3D world and start running it
-    //World *world = new gfxc::SimpleScene();
-
-    //World *world = new m1::Lab1();
-    //World *world = new m1::Lab2();
-    //World *world = new m1::Lab3();
-    //World *world = new m1::Lab3_Vis2D();
-    //World *world = new m1::Lab4();
-    //World *world = new m1::Lab5();
-    //World *world = new m1::Lab6();
-    //World *world = new m1::Lab9();
-    
-    //World *world = new m2::Lab1();
-    World *world = new m2::Lab2();
+    World *world = new m2::Lab3();
 
     world->Init();
     world->Run();
